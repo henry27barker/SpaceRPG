6531fc200ea027f5a94b286f77336745b89b40a0
thbarker <thbarker.griz@gmail.com>
2024-02-24 19:19:21 -0800

    Direction Fixed and Rotation Implemented
    Lil Bro now faces the right way depending on the right analog stick's rotation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class PlayerMovement : MonoBehaviour
     private int direction;
     private SpriteRenderer spriteRenderer;
     public HealthBar healthBar;
+    private Vector2 lookInputValue;
+    public int lookRotation;
+    public float rightStickDeadZone;
  
     void Start()
     {
@@ -20,44 +24,91 @@ public class PlayerMovement : MonoBehaviour
  
     void Update()
     {
-        //Movement
-        float moveHorizontal = Input.GetAxisRaw ("Horizontal");
-        float moveVertical = Input.GetAxisRaw ("Vertical");
+        MovementLogic();
+        
+        UpdateLookRotation();
 
-        rb2d.velocity = new Vector2(moveHorizontal, moveVertical).normalized * new Vector2(speed, speed);
 
-        Debug.Log(direction);
-
-        //Animation
         UpdateAnimations();
 
-        //HealthBar Updates
+        
         UpdateHealthBar();
+    }
+
+    private void OnLook(InputValue value)
+    {
+        lookInputValue = value.Get<Vector2>();
+    }
+
+    private void MovementLogic()
+    {
+        //Movement
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        rb2d.velocity = new Vector2(moveHorizontal, moveVertical).normalized * new Vector2(speed, speed);
+    }
+
+    private void UpdateLookRotation()
+    {
+        Debug.Log(lookInputValue);
+        if(Mathf.Abs(lookInputValue[0]) > rightStickDeadZone || Mathf.Abs(lookInputValue[1]) > rightStickDeadZone)
+        {
+            // Get input from the right stick
+            float rightStickX = lookInputValue[0];
+            float rightStickY = lookInputValue[1];
+
+            // Calculate the angle in radians
+            float angleRadians = Mathf.Atan2(rightStickY, rightStickX);
+
+            // Convert radians to degrees
+            float angleDegrees = angleRadians * Mathf.Rad2Deg;
+
+            // Convert negative angles to positive equivalent
+            if (angleDegrees < 0)
+            {
+                angleDegrees += 360f;
+            }
+
+            // Round to nearest integer
+            lookRotation = Mathf.RoundToInt(angleDegrees);
+
+            // Output the angle as an integer
+            //Debug.Log("Right Stick Angle (Degrees): " + lookRotation);
+        }
     }
 
     private void UpdateAnimations()
     {
-        if (Mathf.Abs(rb2d.velocity.x) >= Mathf.Abs(rb2d.velocity.y) && rb2d.velocity.x != 0)
+        /*Directions:
+         * 0 = Right
+         * 1 = UpRight
+         * 2 = DownLeft
+         * 3 = Left
+         */
+
+        if (lookRotation > 315 || lookRotation <= 45)
         {
-            if (rb2d.velocity.x > 0)
-            {
-                direction = 0;
-            }
-            else
-            {
-                direction = -1;
-            }
+            direction = 0;
+            animator.SetBool("FacingDown", true);
         }
-        else if (rb2d.velocity.y > 0)
+        else if (lookRotation > 45 && lookRotation < 90)
         {
             direction = 1;
+            animator.SetBool("FacingDown", false);
         }
-        else if (rb2d.velocity.y < 0)
+        else if(lookRotation > 90 && lookRotation < 135)
         {
             direction = 2;
+            animator.SetBool("FacingDown", false);
+        }
+        else
+        {
+            direction = 3;
+            animator.SetBool("FacingDown", true);
         }
 
-        if (direction == 0)
+        if (direction < 2)
         {
             spriteRenderer.flipX = false;
         }
@@ -74,7 +125,6 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetFloat("Speed", 0);
         }
-        animator.SetInteger("Direction", direction);
     }
 
     private void UpdateHealthBar()
@@ -90,6 +140,7 @@ public class PlayerMovement : MonoBehaviour
             health += 10;
         }
         //Direction Update
+        /*
         Vector3 playerPosition = new Vector3();
         Vector3 vectorToAdd;
         playerPosition = transform.position;
@@ -111,6 +162,6 @@ public class PlayerMovement : MonoBehaviour
                 vectorToAdd = new Vector3(0f, 0f, 0f);
                 healthBar.transform.position = playerPosition + vectorToAdd;
                 break;
-        }
+        }*/
     }
 }
