2a5887a5ce7978cda22ffbc423e9e83e43a78e27
thbarker <thbarker.griz@gmail.com>
2024-02-26 21:43:44 -0800

    Animations added to Blow Up Guy
    no collision on blow up guy still

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -14,38 +14,63 @@ public class EnemyMovement : MonoBehaviour
     public GameObject player;
     public PlayerMovement playerController;
     private float distance;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    private Vector2 lastPosition;
  
     void Start()
     {
         deathRadiusReached = false;
         playerController = player.GetComponent<PlayerMovement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        lastPosition = transform.position;
     }
- 
+
     void Update()
     {
-        if(health <= 0){
+
+        if (health <= 0) {
             Destroy(gameObject);
         }
 
         distance = Vector2.Distance(transform.position, player.transform.position);
 
-        if(distance <= deathRadius){
+        if (distance <= deathRadius) {
             deathRadiusReached = true;
+            animator.SetBool("Explode", true);
         }
 
-        if(deathRadiusReached && deathTime > 0){
+        if (deathRadiusReached && deathTime > 0) {
             deathTime -= Time.deltaTime;
         }
-        else if(deathTime <= 0){
-            if(distance <= explosionRadius){
+        else if (deathTime <= 0) {
+            if (distance <= explosionRadius) {
                 playerController.decreaseHealth(damage);
             }
             Destroy(gameObject);
         }
-        else{
+        else {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         }
+
+
+        //BELOW: Flips the Sprite Based on movement direction
+        if (lastPosition[0] < transform.position[0])
+        {
+            Debug.Log("Going Right");
+            spriteRenderer.flipX = false;
+        } 
+        else if (lastPosition[0] > transform.position[0])
+        {
+            Debug.Log("Going Left");
+            spriteRenderer.flipX = true;
+        }
+
+        lastPosition = transform.position;
+
     }
+        
 
     public void decreaseHealth(int damage){
         health -= damage;
