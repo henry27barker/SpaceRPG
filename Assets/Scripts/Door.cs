79e4f31bd803c37ce4972cd485e3ace385a2ea1a
thbarker <thbarker.griz@gmail.com>
2024-04-09 11:17:53 -0700

    Door, Collision
    Door opens when interacted with.
    Scene Changes when Door is entered.
    Player Collision is now polygon

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Door : Interactable
 {
     public bool unlocked;
+    public Sprite doorOpen;
+    public Sprite doorClosed;
+    SpriteRenderer spriteRenderer;
+    public GameObject openCollisionBox;
+    private BoxCollider2D closedCollisionBox;
 
     public override void Interact()
     {
@@ -19,13 +24,17 @@ public class Door : Interactable
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        closedCollisionBox = GetComponent<BoxCollider2D>();
     }
 
 
 
     private void OpenDoor()
     {
+        spriteRenderer.sprite = doorOpen;
+        closedCollisionBox.enabled = false;
+        openCollisionBox.SetActive(true);
         Debug.Log("Open Door");
     }
 }
