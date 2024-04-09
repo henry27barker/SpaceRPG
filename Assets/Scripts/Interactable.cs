d24297968daf22a30b42ae4b0c0c17bbf004a289
thbarker <thbarker.griz@gmail.com>
2024-04-08 21:02:47 -0700

    Xbox button Prompts

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -7,21 +7,9 @@ public class Interactable : MonoBehaviour
     bool isFocus = false;
     Transform player;
 
-    public GameObject playerObject;
-
     bool hasInteracted = false;
 
     public float radius = 5f;
-    public GameObject promptIcon;
-    private GameObject currentIcon;
-    public PlayerMovement playerMovement;
-
-    private bool promptActive = false;
-
-    void Start(){
-        playerMovement = playerObject.GetComponent<PlayerMovement>();
-        radius = playerMovement.pickupRadius;
-    }
 
     public virtual void Interact(){
         Debug.Log("interacting with " + transform.name);
@@ -34,9 +22,6 @@ public class Interactable : MonoBehaviour
                 Interact();
                 hasInteracted = true;
             //}
-        }
-        else if(hasInteracted || Vector2.Distance(new Vector2(playerObject.transform.position.x, playerObject.transform.position.y), new Vector2(transform.position.x, transform.position.y)) > radius){
-            DeactivatePrompt();
         }
     }
 
@@ -54,24 +39,8 @@ public class Interactable : MonoBehaviour
 
     public void Prompt()
     {
-        if (!hasInteracted)
-        {
-            //Debug.Log("Press X to Interact");
-        }
-        if(promptActive == false){
-            Debug.Log("SPRITE ENABLED FOR " + transform.name);
-            currentIcon = Instantiate(promptIcon, transform);
-            promptActive = true;
-        }
-        //ENABLE A PROMPT SPRITE
     }
 
-    public void DeactivatePrompt(){
-        if(promptActive == true){
-            Destroy(currentIcon);
-            promptActive = false;
-        }
-    }
 
     void OnDrawGizmosSelected(){
         Gizmos.color = Color.yellow;
