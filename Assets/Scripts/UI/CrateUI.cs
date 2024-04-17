3ec59845401b9433626d9736269f5142c562ee45
henry27barker <henry27barker@gmail.com>
2024-04-16 21:24:00 -0700

    crate randomness

--- Assets/Scripts/UI/CrateUI.cs
+++ Assets/Scripts/UI/CrateUI.cs
@@ -7,14 +7,27 @@ public class CrateUI : MonoBehaviour
     public GameObject inventoryUI;
     public GameObject crateUI;
     public List<Item> items = new List<Item>();
+    public List<Item> possibleItems = new List<Item>();
     public Transform itemsParent;
     CrateSlot[] slots;
+
+    public int maxPossibleItems;
 
     public int space = 10;
 
     void Awake(){
         crateUI.SetActive(false);
         slots = itemsParent.GetComponentsInChildren<CrateSlot>();
+        UpdateUI();
+    }
+
+    void Start(){
+        Random.seed = System.DateTime.Now.Millisecond;
+        int size = Random.Range(0, maxPossibleItems);
+        for(int i = 0; i <= size; i++){
+            int index = Random.Range(0, possibleItems.Count);
+            items.Add(possibleItems[index]);
+        }
         UpdateUI();
     }
 
