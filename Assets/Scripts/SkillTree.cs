0816f170cbce3e6737957788baa1893a189cf0af
henry27barker <henry27barker@gmail.com>
2024-05-21 12:34:26 -0700

    skill tree implemented with life steal and speed

--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -10,12 +10,16 @@ public class SkillTree : MonoBehaviour
     private GameObject skillTreeUI;
     private TMP_Text maxHealthText;
     private TMP_Text upgradeTokensText;
+    private TMP_Text speedText;
+    private TMP_Text lifeStealText;
 
     public int upgradeTokens;
     public int maxHealth;
     public int minHealth;
     public float speed;
+    public float minSpeed;
     public float lifeSteal;
+    public float minLifeSteal;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,8 @@ public class SkillTree : MonoBehaviour
         inventory = GameObject.FindWithTag("GameManager").GetComponent<Inventory>();
         skillTreeUI = GameObject.FindWithTag("SkillTree");
         maxHealthText = skillTreeUI.transform.Find("MaxHealth/MaxHealthPanel/MaxHealthBackgroundPanel/MaxHealthTextNumber").gameObject.GetComponent<TMP_Text>();
+        speedText = skillTreeUI.transform.Find("Speed/SpeedPanel/SpeedBackgroundPanel/SpeedTextNumber").gameObject.GetComponent<TMP_Text>();
+        lifeStealText = skillTreeUI.transform.Find("LifeSteal/LifeStealPanel/LifeStealBackgroundPanel/LifeStealTextNumber").gameObject.GetComponent<TMP_Text>();
         upgradeTokensText = skillTreeUI.transform.Find("UpgradeTokensPanel/UpgradeTokensBackgroundPanel/UpgradeTokensTextNumber").gameObject.GetComponent<TMP_Text>();
         skillTreeUI.SetActive(false);
     }
@@ -34,6 +40,8 @@ public class SkillTree : MonoBehaviour
         upgradeTokensText.text = upgradeTokens.ToString();
         playerMovement.maxHealth = maxHealth;
         maxHealthText.text = maxHealth.ToString();
+        speedText.text = speed.ToString();
+        lifeStealText.text = lifeSteal.ToString();
         playerMovement.speed = speed;
         playerMovement.lifeSteal = lifeSteal;
     }
@@ -68,10 +76,24 @@ public class SkillTree : MonoBehaviour
         }
     }
 
+    public void DecrementSpeed(float decrementAmount){
+        if(speed > minSpeed){
+            speed -= decrementAmount;
+            upgradeTokens++;
+        }
+    }
+
     public void IncrementLifeSteal(float incrementAmount){
         if(upgradeTokens > 0){
             lifeSteal += incrementAmount;
             upgradeTokens--;
+        }
+    }
+
+    public void DecrementLifeSteal(float decrementAmount){
+        if(lifeSteal > minLifeSteal){
+            lifeSteal -= decrementAmount;
+            upgradeTokens++;
         }
     }
 }
