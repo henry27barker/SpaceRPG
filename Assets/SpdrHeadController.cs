9ba52e0804efccb2e79e7ec6cdd56028437ba4c4
thbarker <thbarker.griz@gmail.com>
2024-06-10 14:02:54 -0700

    SPDR Bot shooting complete

--- Assets/SpdrHeadController.cs
+++ Assets/SpdrHeadController.cs
@@ -7,6 +7,27 @@ public class SpdrHeadController : MonoBehaviour
     public GameObject player;
     public GameObject projectile;
     public Transform shootingPoint;
+    public SpriteRenderer spriteRenderer;
+
+    //Head Sprites
+    public Sprite angle_0;
+    public Sprite angle_45;
+    public Sprite angle_90;
+    public Sprite angle_135;
+    public Sprite angle_180;
+    public Sprite angle_225;
+    public Sprite angle_270;
+    public Sprite angle_315;
+    //Shooting Points
+    public Vector3 offSets_0;
+    public Vector3 offSets_45;
+    public Vector3 offSets_90;
+    public Vector3 offSets_135;
+    public Vector3 offSets_180;
+    public Vector3 offSets_225;
+    public Vector3 offSets_270;
+    public Vector3 offSets_315;
+
 
     private float counter;
     public float fireRate;
@@ -14,7 +35,7 @@ public class SpdrHeadController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -42,6 +63,8 @@ public class SpdrHeadController : MonoBehaviour
         Debug.Log(lookRotation);
         shootingPoint.rotation = Quaternion.Euler(0, 0, lookRotation);
 
+        UpdateSprite(lookRotation);
+
         Shoot();
     }
 
@@ -57,5 +80,49 @@ public class SpdrHeadController : MonoBehaviour
             counter = fireRate;
         }
         
+    }
+
+    private void UpdateSprite(int degrees)
+    {
+        if(degrees < 22 || degrees > 337)
+        {
+            spriteRenderer.sprite = angle_0;
+            shootingPoint.position = transform.position + offSets_0;
+        }
+        else if(degrees < 67)
+        {
+            spriteRenderer.sprite = angle_45;
+            shootingPoint.position = transform.position + offSets_45;
+        }
+        else if (degrees < 112)
+        {
+            spriteRenderer.sprite = angle_90;
+            shootingPoint.position = transform.position + offSets_90;
+        }
+        else if (degrees < 157)
+        {
+            spriteRenderer.sprite = angle_135;
+            shootingPoint.position = transform.position + offSets_135;
+        }
+        else if (degrees < 202)
+        {
+            spriteRenderer.sprite = angle_180;
+            shootingPoint.position = transform.position + offSets_180;
+        }
+        else if (degrees < 247)
+        {
+            spriteRenderer.sprite = angle_225;
+            shootingPoint.position = transform.position + offSets_225;
+        }
+        else if (degrees < 292)
+        {
+            spriteRenderer.sprite = angle_270;
+            shootingPoint.position = transform.position + offSets_270;
+        }
+        else if (degrees < 337)
+        {
+            spriteRenderer.sprite = angle_315;
+            shootingPoint.position = transform.position + offSets_315;
+        }
     }
 }
