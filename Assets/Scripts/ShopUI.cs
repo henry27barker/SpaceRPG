7733c9700420b9f1988893f2b36b7ebc19d00688
henry27barker <henry27barker@gmail.com>
2024-06-14 14:10:51 -0700

    implementing pricing to shop

--- Assets/Scripts/ShopUI.cs
+++ Assets/Scripts/ShopUI.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShopUI : MonoBehaviour
 {
     private PlayerMovement playerMovement;
 
     public int ammoPrice;
-    public int ammoAmountPerPrice;
+    private TMP_Text ammoText;
 
     void Awake(){
         playerMovement = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+        ammoText = gameObject.transform.Find("Ammo/AmmoPanel/AmmoBackgroundPanel/AmmoTextNumber").gameObject.GetComponent<TMP_Text>();
+        ammoText.text = "$" + ammoPrice.ToString();
     }
 
     // Start is called before the first frame update
