b73196e9c53aac1ace755006d9bfe0531c6ade49
henry27barker <henry27barker@gmail.com>
2024-07-12 14:42:44 -0700

    more random rooms

--- Assets/Scripts/RandomRooms/RoomTemplates.cs
+++ Assets/Scripts/RandomRooms/RoomTemplates.cs
@@ -8,7 +8,14 @@ public class RoomTemplates : MonoBehaviour
     public GameObject[] topRooms;
     public GameObject[] leftRooms;
     public GameObject[] rightRooms;
+
+    public GameObject[] allRoomTypes;
     //public GameObject wall;
+
+    //1->top
+    //2->right
+    //3->bottom
+    //4->left
 
     public GameObject[,] rooms;
 
@@ -16,6 +23,7 @@ public class RoomTemplates : MonoBehaviour
     public int rowHeight = 20;
 
     private int rand;
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,41 +40,84 @@ public class RoomTemplates : MonoBehaviour
     }
 
     void MakeLevel(int columnPos, int rowPos){
+        Debug.Log(columnPos + " " + rowPos);
         if(columnPos == columnHeight / 2 && rowPos == rowHeight / 2){
-            rand = Random.Range(0, 4);
-            GameObject[] temp;
-            if(rand == 0){
-                temp = bottomRooms;
+            rand = Random.Range(0, allRoomTypes.Length);
+            rooms[columnPos, rowPos] = Instantiate(allRoomTypes[rand], transform.position, Quaternion.identity);
+            if(rooms[columnPos, rowPos].GetComponent<RoomType>().openingDirections.Contains(1) && rooms[columnPos, rowPos + 1] == null){
+                MakeLevel(columnPos, rowPos + 1);
             }
-            else if(rand == 1){
-                temp = topRooms;
+            if(rooms[columnPos, rowPos].GetComponent<RoomType>().openingDirections.Contains(2) && rooms[columnPos + 1, rowPos] == null){
+                MakeLevel(columnPos + 1, rowPos);
             }
-            else if(rand == 2){
-                temp = leftRooms;
+            if(rooms[columnPos, rowPos].GetComponent<RoomType>().openingDirections.Contains(3) && rooms[columnPos, rowPos - 1] == null){
+                MakeLevel(columnPos, rowPos - 1);
             }
-            else{
-                temp = rightRooms;
-            }
-            rand = Random.Range(0, temp.Length);
-            rooms[columnPos, rowPos] = Instantiate(temp[rand], transform.position, Quaternion.identity);
-            for(int i = 0; i < temp[rand].GetComponent<RoomType>().openingDirections.Count; i++){
-                if(temp[rand].GetComponent<RoomType>().openingDirections[i] == 1){
-                    MakeLevel(columnPos, rowPos + 1);
-                }
-                if(temp[rand].GetComponent<RoomType>().openingDirections[i] == 2){
-                    MakeLevel(columnPos + 1, rowPos);
-                }
-                if(temp[rand].GetComponent<RoomType>().openingDirections[i] == 3){
-                    MakeLevel(columnPos, rowPos - 1);
-                }
-                if(temp[rand].GetComponent<RoomType>().openingDirections[i] == 4){
-                    MakeLevel(columnPos - 1, rowPos);
-                }
+            if(rooms[columnPos, rowPos].GetComponent<RoomType>().openingDirections.Contains(4) && rooms[columnPos - 1, rowPos] == null){
+                MakeLevel(columnPos - 1, rowPos);
             }
         }
-        else if(columnPos < columnHeight && columnPos > 0 && rowPos < rowHeight && rowPos > 0){
+        else if(columnPos < columnHeight - 1 && columnPos > 0 && rowPos < rowHeight - 1 && rowPos > 0){
+            List<int> needOpening = new List<int>();
+            List<int> needClosed = new List<int>();
             if(rooms[columnPos, rowPos + 1] != null && rooms[columnPos, rowPos + 1].GetComponent<RoomType>().openingDirections.Contains(3)){
-                //Instantiate()
+                needOpening.Add(1);
+            }
+            if(rooms[columnPos, rowPos + 1] != null && !rooms[columnPos, rowPos + 1].GetComponent<RoomType>().openingDirections.Contains(3)){
+                needClosed.Add(1);
+            }
+            if(rooms[columnPos + 1, rowPos] != null && rooms[columnPos + 1, rowPos].GetComponent<RoomType>().openingDirections.Contains(4)){
+                needOpening.Add(2);
+            }
+            if(rooms[columnPos + 1, rowPos] != null && !rooms[columnPos + 1, rowPos].GetComponent<RoomType>().openingDirections.Contains(4)){
+                needClosed.Add(2);
+            }
+            if(rooms[columnPos, rowPos - 1] != null && rooms[columnPos, rowPos - 1].GetComponent<RoomType>().openingDirections.Contains(1)){
+                needOpening.Add(3);
+            }
+            if(rooms[columnPos, rowPos - 1] != null && !rooms[columnPos, rowPos - 1].GetComponent<RoomType>().openingDirections.Contains(1)){
+                needClosed.Add(3);
+            }
+            if(rooms[columnPos - 1, rowPos] != null && rooms[columnPos - 1, rowPos].GetComponent<RoomType>().openingDirections.Contains(2)){
+                needOpening.Add(4);
+            }
+            if(rooms[columnPos - 1, rowPos] != null && !rooms[columnPos - 1, rowPos].GetComponent<RoomType>().openingDirections.Contains(2)){
+                needClosed.Add(4);
+            }
+            List<GameObject> possibleRooms = new List<GameObject>();
+            foreach(GameObject temp in allRoomTypes){
+                foreach(int i in needOpening){
+                    if(!temp.GetComponent<RoomType>().openingDirections.Contains(i)){
+                        goto EndLoop;
+                    }
+                }
+                foreach(int i in needClosed){
+                    if(temp.GetComponent<RoomType>().openingDirections.Contains(i)){
+                        goto EndLoop;
+                    }
+                }
+                possibleRooms.Add(temp);
+                EndLoop:
+                    ;
+            }
+            if(possibleRooms.Count != 0){
+                rand = Random.Range(0, possibleRooms.Count);
+                int columnAdjustment = (columnPos - columnHeight / 2) * 4;
+                int rowAdjustment = (rowPos - rowHeight / 2) * 4;
+                Vector3 newPosition = new Vector3(transform.position.x + columnAdjustment, transform.position.y + rowAdjustment, 0);
+                rooms[columnPos, rowPos] = Instantiate(possibleRooms[rand], newPosition, Quaternion.identity);
+                if(rooms[columnPos, rowPos].GetComponent<RoomType>().openingDirections.Contains(1) && rooms[columnPos, rowPos + 1] == null){
+                    MakeLevel(columnPos, rowPos + 1);
+                }
+                if(rooms[columnPos, rowPos].GetComponent<RoomType>().openingDirections.Contains(2) && rooms[columnPos + 1, rowPos] == null){
+                    MakeLevel(columnPos + 1, rowPos);
+                }
+                if(rooms[columnPos, rowPos].GetComponent<RoomType>().openingDirections.Contains(3) && rooms[columnPos, rowPos - 1] == null){
+                    MakeLevel(columnPos, rowPos - 1);
+                }
+                if(rooms[columnPos, rowPos].GetComponent<RoomType>().openingDirections.Contains(4) && rooms[columnPos - 1, rowPos] == null){
+                    MakeLevel(columnPos - 1, rowPos);
+                }
             }
         }
     }
