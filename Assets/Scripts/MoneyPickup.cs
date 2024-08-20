ac033b32adc5b110624adb5d352b7183ebc96296
thbarker <thbarker.griz@gmail.com>
2024-08-20 10:31:56 -0700

    Money is Attracted to Player

--- Assets/Scripts/MoneyPickup.cs
+++ Assets/Scripts/MoneyPickup.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class MoneyPickup : MonoBehaviour
 {
     Collider2D collider;
 
+    private bool go;
+    private GameObject player;
+
     public int amount;
 
     void Awake(){
         collider = GetComponent<Collider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        Vector2 direction = player.transform.position - transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 2);
+        Debug.DrawLine(transform.position, hit.point);
+        if(hit.collider != null)
+        {
+            if(hit.collider.tag == "Player")
+            {
+                go = true;
+            }
+        }
+        if(go)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, 5 * Time.deltaTime);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col){
